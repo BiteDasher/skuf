847baea5358d71a5eec94c02b41ce74e487a2847
BiteDasher <finziyr@yandex.ru>
2024-02-14 20:14:25 +0300

    fix quotes in setup_defaults.sh

--- setup_defaults.sh
+++ setup_defaults.sh
@@ -42,80 +42,81 @@ fi
 
 [ -f ./defaults ] && source ./defaults
 
+# "'" - very cool! Thanks, GNU!
 if [ -n "$SAMBA_ADDRESS" ]; then
-    sed -i 's|# SAMBA_ADDRESS #|-i "'$SAMBA_ADDRESS'" smbaddr|' ./skuf_src/kinit
+    sed -i 's|# SAMBA_ADDRESS #|-i "'"$SAMBA_ADDRESS"'" smbaddr|' ./skuf_src/kinit
 else
     sed -i 's|# SAMBA_ADDRESS #|smbaddr|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$SAMBA_PORT" ]; then
-    sed -i 's|# SAMBA_PORT #|-i "'$SAMBA_PORT'" smbport|' ./skuf_src/kinit
+    sed -i 's|# SAMBA_PORT #|-i "'"$SAMBA_PORT"'" smbport|' ./skuf_src/kinit
 else
     sed -i 's|# SAMBA_PORT #|smbport|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$SAMBA_VERSION" ]; then
-    sed -i 's|# SAMBA_VERSION #|-i "'$SAMBA_VERSION'" smbversion|' ./skuf_src/kinit
+    sed -i 's|# SAMBA_VERSION #|-i "'"$SAMBA_VERSION"'" smbversion|' ./skuf_src/kinit
 else
     sed -i 's|# SAMBA_VERSION #|smbversion|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$SAMBA_DOMAIN" ]; then
-    sed -i 's|# SAMBA_DOMAIN #|-i "'$SAMBA_DOMAIN'" smbdomain|' ./skuf_src/kinit
+    sed -i 's|# SAMBA_DOMAIN #|-i "'"$SAMBA_DOMAIN"'" smbdomain|' ./skuf_src/kinit
 else
     sed -i 's|# SAMBA_DOMAIN #|smbdomain|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$VOLUME_PATH" ]; then
-    sed -i 's|# VOLUME_PATH #|-i "'$VOLUME_PATH'" smbvolumepath|' ./skuf_src/kinit
+    sed -i 's|# VOLUME_PATH #|-i "'"$VOLUME_PATH"'" smbvolumepath|' ./skuf_src/kinit
 else
     sed -i 's|# VOLUME_PATH #|smbvolumepath|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$VOLUME_FILENAME" ]; then
-    sed -i 's|# VOLUME_FILENAME #|-i "'$VOLUME_FILENAME'" smbvolumefilename|' ./skuf_src/kinit
+    sed -i 's|# VOLUME_FILENAME #|-i "'"$VOLUME_FILENAME"'" smbvolumefilename|' ./skuf_src/kinit
 else
     sed -i 's|# VOLUME_FILENAME #|smbvolumefilename|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$SWAP_FILENAME" ]; then
-    sed -i 's|# SWAP_FILENAME #|-i "'$SWAP_FILENAME'" smbswapfilename|' ./skuf_src/kinit
+    sed -i 's|# SWAP_FILENAME #|-i "'"$SWAP_FILENAME"'" smbswapfilename|' ./skuf_src/kinit
 else
     sed -i 's|# SWAP_FILENAME #|smbswapfilename|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$SAMBA_EXTRA_MOUNT_OPTS" ]; then
-    sed -i 's|# SAMBA_EXTRA_MOUNT_OPTS #|-i "'$SWAP_EXTRA_MOUNT_OPTS'" smbmountopts|' ./skuf_src/kinit
+    sed -i 's|# SAMBA_EXTRA_MOUNT_OPTS #|-i "'"$SWAP_EXTRA_MOUNT_OPTS"'" smbmountopts|' ./skuf_src/kinit
 else
     sed -i 's|# SAMBA_EXTRA_MOUNT_OPTS #|smbmountopts|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$VOLUME_EXTRA_MOUNT_OPTS" ]; then
-    sed -i 's|# VOLUME_EXTRA_MOUNT_OPTS #|-i "'$VOLUME_EXTRA_MOUNT_OPTS'" newrootmountopts|' ./skuf_src/kinit
+    sed -i 's|# VOLUME_EXTRA_MOUNT_OPTS #|-i "'"$VOLUME_EXTRA_MOUNT_OPTS"'" newrootmountopts|' ./skuf_src/kinit
 else
     sed -i 's|# VOLUME_EXTRA_MOUNT_OPTS #|newrootmountopts|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$CHECK_FS" ]; then
-    sed -i 's|# CHECK_FS #|-i "'$CHECK_FS'" newrootfsck|' ./skuf_src/kinit
+    sed -i 's|# CHECK_FS #|-i "'"$CHECK_FS"'" newrootfsck|' ./skuf_src/kinit
 else
     sed -i 's|# CHECK_FS #|newrootfsck|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$EXTRA_KERNEL_OPTS" ]; then
-    sed -i 's|# EXTRA_KERNEL_OPTS #|-i "'$EXTRA_KERNEL_OPTS'" kernelopts|' ./skuf_src/kinit
+    sed -i 's|# EXTRA_KERNEL_OPTS #|-i "'"$EXTRA_KERNEL_OPTS"'" kernelopts|' ./skuf_src/kinit
 else
     sed -i 's|# EXTRA_KERNEL_OPTS #|kernelopts|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$PATH_TO_NEW_KERNEL" ]; then
-    sed -i 's|# PATH_TO_NEW_KERNEL #|-i "'$PATH_TO_NEW_KERNEL'" kernelpath|' ./skuf_src/kinit
+    sed -i 's|# PATH_TO_NEW_KERNEL #|-i "'"$PATH_TO_NEW_KERNEL"'" kernelpath|' ./skuf_src/kinit
 else
     sed -i 's|# PATH_TO_NEW_KERNEL #|kernelpath|' ./skuf_src/kinit
 fi
 ##################################################
 if [ -n "$PATH_TO_NEW_INITRAMFS" ]; then
-    sed -i 's|# PATH_TO_NEW_INITRAMFS #|-i "'$PATH_TO_NEW_INITRAMFS'" initramfspath|' ./skuf_src/kinit
+    sed -i 's|# PATH_TO_NEW_INITRAMFS #|-i "'"$PATH_TO_NEW_INITRAMFS"'" initramfspath|' ./skuf_src/kinit
 else
     sed -i 's|# PATH_TO_NEW_INITRAMFS #|initramfspath|' ./skuf_src/kinit
 fi
